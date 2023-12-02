a78331138ca80c14db77a1c8e1e3be25b1f27ec3
Gurmukh Chandan <gsc128@gmail.com>
2023-12-02 17:05:09 +0000

    test AoC

---
+++ AoC_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var sr = new StreamReader("input.txt");
+        var line = string.Empty;
+        var sumTotal = 0;
+        while ((line = sr.ReadLine()) is not null)
+        {
+            sumTotal += ParseLine(line.Where(char.IsDigit).ToArray());
+            Console.WriteLine(sumTotal);
+        }
+
+
+        sr.Close();
+        Console.ReadLine();
+    }
+
+    private static int ParseLine(char[] line)
+    {
+        Console.WriteLine($"{line.FirstOrDefault()}, {line.LastOrDefault()}");
+        return 1;
+    }
+}
